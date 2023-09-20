8cd8ac60c7006ca6d8ac6113da3cca733a881882
Mahmoud AlEbrahim <ma453935@gmail.com>
2023-09-20 18:22:11 +0300

    .

--- lib/features/authentication/domain/repositories/auth_repository.dart
+++ lib/features/authentication/domain/repositories/auth_repository.dart
@@ -1,12 +1,10 @@
 import 'package:dartz/dartz.dart';
-
 import '../../../../core/error/failures.dart';
 import '../../data/models/create_user_response_model.dart';
 import '../../data/models/login_user_response_model.dart';
 import '../../data/models/send_otp_response_model.dart';
 import '../../data/models/store_fcm_token_response_model.dart';
 import '../../data/models/verify_guest_phone_response_model.dart';
-import '../../data/models/verify_otp_response_model.dart';
 import '../../data/models/verify_otp_sign_up_and_in_response_model.dart';
 
 abstract class AuthRepository {
